65309feafeeff5f45d749180692173db2c12c9f4
Tom Österlund <tom_jonathan.oesterlund@prosiebensat1.com>
2024-01-16 06:06:48 +0100

    first commit

---
+++ components/app-calendar.vue
@@ -0,0 +1,55 @@
+<script setup>
+import {ScheduleXCalendar} from "@schedule-x/vue";
+import {
+  createCalendar,
+  viewDay,
+  viewWeek,
+  viewMonthGrid,
+  viewMonthAgenda,
+} from '@schedule-x/calendar'
+import '@schedule-x/theme-default/dist/index.css'
+
+const calendarApp = createCalendar({
+  selectedDate: '2023-12-19',
+  views: [viewDay, viewWeek, viewMonthGrid, viewMonthAgenda],
+  defaultView: viewWeek.name,
+  events: [
+    {
+      id: 1,
+      title: 'Event 1',
+      start: '2023-12-19',
+      end: '2023-12-19',
+    },
+    {
+      id: 2,
+      title: 'Event 2',
+      start: '2023-12-20 12:00',
+      end: '2023-12-20 13:00',
+    },
+  ],
+  calendars: {
+    work: {
+      colorName: 'work',
+      lightColors: {
+        container: '#fff',
+        onContainer: '#000',
+        main: '#fff',
+      },
+      darkColors: {
+        container: '#000',
+        onContainer: '#fff',
+        main: '#000',
+      },
+    }
+  }
+})
+
+</script>
+
+<template>
+  <div>
+    <ClientOnly>
+      <ScheduleXCalendar :calendar-app="calendarApp" />
+    </ClientOnly>
+  </div>
+</template>
